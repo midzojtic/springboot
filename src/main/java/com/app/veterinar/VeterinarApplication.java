560bda653b3a7def2d574fe2ff200fc1791767d5
Marko <midzojtic@gmail.com>
2019-04-26 11:14:06 +0200

    init

---
+++ src/main/java/com/app/veterinar/VeterinarApplication.java
@@ -0,0 +1,13 @@
+package com.app.veterinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinarApplication.class, args);
+    }
+
+}
